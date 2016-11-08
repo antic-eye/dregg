163b705697ed3c5a7dfd0e76fbd8650ff1998e8d
Sven Rütz <ruetz@itisim.com>
2016-11-08 14:23:46 +0100

    move functionality to extra class

--- Program.cs
+++ Program.cs
@@ -45,47 +45,13 @@ namespace dregg
 
             if (parseResult.Succeeded)
             {
-                //strip rpc url
-                string server = host.Replace("/login/jsonrpc", string.Empty).Replace("/jsonrpc", string.Empty);
-
-                Api trac = new Api(host, user, password);
-                NameValueCollection col = new NameValueCollection();
-                if (bClosedOnly)
-                    col.Add("status", "closed");
-                col.Add("milestone", milestone);
-                col.Add("order", "id");
-
-                string sPath = Path.GetTempFileName().Replace(".tmp", ".csv");
-                using (TextWriter writer = new StreamWriter(sPath, false, Encoding.Default))
-                {
-                    writer.WriteLine("\"Ticket\";\"TicketAuthor\";\"Summary\";\"Release Note Content\";\"Source\"");
-                    for (int i = 1; i < 1000; i++)
-                    {
-                        var query = trac.QueryTickets(col, i);
-
-                        if (null == query || null == query.Result || query.Result.Length==0)
-                            break;
-
-                        Trace.WriteLine("Processing page " + i);
-                        foreach (var res in query.Result)
-                        {
-                            var ticket = trac.GetTicket(res);
-                            var changes = trac.GetChanges(res).ChangeList;
-                            var r = (from f in changes where f.Action.ToLowerInvariant() == "comment" select f).Distinct();
-                            List<int> lHashs = new List<int>();
-                            foreach (var o in r)
-                            {
-                                if (lHashs.Contains(ticket.Data.Summary.GetHashCode()))
-                                    continue;
-                                else
-                                    lHashs.Add(ticket.Data.Summary.GetHashCode());
-                                writer.WriteLine("\"=HYPERLINK(\"\"{0}/ticket/{1}\"\"; \"\"#{1}\"\")\";\"{2}\";\"{3}\";\"{4}\";\"{5}\"",
-                                    server, res, o.Author, ticket.Data.Summary, o.To, ticket.Data.Source);
-                            }
-                        }
-                    }
-                }
-                Process.Start(sPath);
+                RpcCall call = new RpcCall();
+                call.closedOnly = bClosedOnly;
+                call.host = host;
+                call.user = user;
+                call.password = password;
+                call.milestone = milestone;
+                call.DoCall();
             }
             else
             {
